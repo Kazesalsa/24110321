57252e49f821b266d8028ae96a3689c2ca061e34
Pham Tan Tai <tv2952018@gmail.com>
2026-05-28 10:15:00 +0700

    Heuristic Generation

--- algorithms/informed.py
+++ algorithms/informed.py
@@ -73,3 +73,4 @@ def solve_best_first(start_board, goal_board):
             
     return None, nodes_expanded, time.time() - start_time
 # Bổ sung đánh giá chi phí f = g + h
+# Tạo và tinh chỉnh hàm Heuristic
